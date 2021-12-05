80f646474ad4bca6898a32dfe2f53704e695c2c0
caiman <me@caiman.dev>
2021-12-05 19:30:17 +0200

    [master] Pyramid

---
+++ pyramid1rev.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int total;
+
+    scanf("%d", &total);
+
+    for ( int row = total; row >= 1; row-- ) {
+        for ( int col = 1; col < row; col++ ) {
+            printf("%d ", col);
+        }
+        printf("%d\n", row);
+    }
+
+    return 0;
+}
