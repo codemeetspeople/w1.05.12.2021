735883236d49e7007b66a0e3590caed8dbdb9cd2
caiman <me@caiman.dev>
2021-12-05 18:23:45 +0200

    [master] Nested loops

--- sum.c
+++ sum.c
@@ -4,12 +4,22 @@ int sum(int x, int y) {
     return x + y;
 }
 
+int max2(int x, int y) {
+    if ( x > y ) {
+        return x;
+    }
+    return y;
+}
+
 int main() {
     int a, b;
+    int max;
 
     scanf("%d %d", &a, &b);
 
-    printf("%d\n", sum(a, b));
+    max = max2(a, b);
+
+    printf("%d\n", max);
 
     return 0;
 }
